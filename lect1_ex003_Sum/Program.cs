36050250f716c276f2e288da2586ffee5aeecd7d
VadimTalimonchik <tvadmins@gmail.com>
2022-06-30 17:05:50 +0300

    Random

--- lect1_ex003_Sum/Program.cs
+++ lect1_ex003_Sum/Program.cs
@@ -6,7 +6,17 @@ Console.WriteLine(numberA + numberB);
 */
 
 // через промежуточную переменную
+/*
 int numberA = 33;
 int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
+*/
+
+// применим случаное число
+int numberA = new Random().Next(1,10); // 1 2 3 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
 int result = numberA + numberB;
 Console.WriteLine(result);
