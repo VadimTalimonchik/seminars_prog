327a44191f617ab6271342cd6a671e6d5a1485ca
VadimTalimonchik <tvadmins@gmail.com>
2022-06-30 15:41:07 +0300

    Lecture 1 Exampel 003 Sum

---
+++ lect1_ex003_Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿// сумма двух чисел
+/*
+int numberA = 3;
+int numberB = 5;
+Console.WriteLine(numberA + numberB);
+*/
+
+// через промежуточную переменную
+int numberA = 33;
+int numberB = 5;
+int result = numberA + numberB;
+Console.WriteLine(result);
