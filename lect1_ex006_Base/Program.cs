24ab17145989ef6502b5b0361988ec91d8b59d6e
VadimTalimonchik <tvadmins@gmail.com>
2022-07-01 14:23:36 +0300

    Lecture 1 Exampel 006 Base

---
+++ lect1_ex006_Base/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+int a = 1;
+int b = 4;
+int c = 3;
+int d = 2;
+int e = 6;
+
+int max = a; // определяем переменную в которую кладем значение первой гири
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+*/
+
+int a = 1;
+int b = 4;
+int c = 3;
+int d = 2;
+int e = 6;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
