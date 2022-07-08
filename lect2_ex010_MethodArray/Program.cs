6ba0e1d4a8c89eac74237c3f5d614c2de70eb04d
VadimTalimonchik <tvadmins@gmail.com>
2022-07-08 16:00:03 +0300

    Lecture 2 Example 010 Method Array

---
+++ lect2_ex010_MethodArray/Program.cs
@@ -0,0 +1,38 @@
+﻿// в данном случае, если в массиве будет несколько
+// одинаковых чисел, то алгоритм покажет их все
+/*int[] array = { 1, 12, 31, 4, 15, 18, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    //index = index + 1;
+    index++; // короткая форма
+}
+*/
+
+//
+int[] array = { 1, 12, 31, 4, 15, 18, 16, 17, 18 };
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++; // короткая форма
+}
